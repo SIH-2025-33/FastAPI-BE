f7e61edcc5206c81ec9a0df5d3b4d68c7c827d94
anisha <anisharao9905@gmail.com>
2025-09-14 21:25:06 +0530

    sfat:getstreak and total distance travelled

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from routers import postRoutes, patchRoutes
+from routers import postRoutes, patchRoutes, getRoutes
 
 app = FastAPI()
 
 app.include_router(router=postRoutes.router)
 app.include_router(router=patchRoutes.router)
+app.include_router(router=getRoutes.router)
---
+++ routers/getRoutes.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Annotated
+from sqlalchemy.orm import Session
+from sqlalchemy import select,and_
+
+
+from database import get_database
+from models import User,Trip,TripMode
+
+
+
+db_dependency = Annotated[Session, Depends(get_database)]
+router = APIRouter(prefix="/read", tags=["read"])
+
+@router.get("/user/streak")
+def get_user_streak(user_id: int, db: db_dependency):
+    stmt = select(User).where(User.id == user_id)
+    user = db.execute(stmt).scalars().first()
+    return {"streak":user.streak}
+
+@router.get("/user/distance/{mode}")
+def get_user_distance_travelled(user_id: int, db: db_dependency):
+    stmt = select(TripMode).where(TripMode.mode_name == mode.upper())
+    mode = db.execute(stmt).scalars().first() 
+    stmt = select(Trip).where(and_(Trip.user_id == user_id, Trip.mode_id == mode.id))
+    trips = db.execute(stmt).scalars().all()
+    total_distance_travelled = 0
+    for trip in trips:
+        total_distance_travelled += trip.distance_travelled
+        return {"distance_travelled":distance_travelled}
