2e24f041d438c9f390d1d267d03824600d899ba2
anisha <anisharao9905@gmail.com>
2025-09-20 21:32:15 +0530

    feat:verifying journey and update purpose

--- routers/patchRoutes.py
+++ routers/patchRoutes.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 
 from database import get_database
-from models import User
+from models import User,Journey
+
 
 router = APIRouter(prefix="/update", tags=["update"])
 db_dependency = Annotated[Session, Depends(get_database)]
@@ -23,4 +24,40 @@ def daily_streak(user_id: int, db: db_dependency):
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An error occured: {str(e)}",
+        )
+@router.patch("/verify_journey")
+def verify_journey(journey_id: int, db: db_dependency):
+    """Update is_verified_by_user flag for all trips in a journey"""
+    try:
+        stmt = select(Journey).where(Journey.id == journey_id)
+        journey = db.execute(stmt).scalars().first()
+        journey.is_verified_by_user = True
+
+        db.commit()
+        return {"message": f"Journey {journey_id} verified successfully"}
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occurred: {str(e)}",
+        )
+
+
+@router.patch("/journey_purpose")
+def update_purpose(journey_id: int, purpose: str, db: db_dependency):
+
+    try:
+        stmt = select(Journey).where(Journey.id == journey_id)
+        journey = db.execute(stmt).scalars().first()
+        journey.purpose = purpose
+
+        db.commit()
+        return {"message": "Purpose verified successfully"}
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occurred: {str(e)}",
         )
